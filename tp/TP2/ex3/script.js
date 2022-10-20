c9255f9c990e0e46719a9446633ff92cfd7688b1
Oussema Trabelsi <79041057+aws3ma@users.noreply.github.com>
2022-10-20 23:27:28 +0100

    tp2

--- tp/TP2/ex3/script.js
+++ tp/TP2/ex3/script.js
@@ -1,8 +1,10 @@
+
 const affichage = () => {
     const pseudo = document.getElementById("pseudo").value;
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     if(!pseudo || !email || !password){
+        
         alert("souvenez-vous de vos informations ?")
     }
     else{
@@ -10,6 +12,29 @@ const affichage = () => {
     }
 }
 const fermer = () =>{
-    close();
+    window.close();
     
 }
+const vider = () =>{
+    const pseudo = document.getElementById("pseudo");
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+    const consfirmPassword = document.getElementById("confirmPassword");
+    pseudo.value=''
+    email.value=''
+    password.value=''
+    consfirmPassword.value=''
+
+}
+swal({
+    title: "Are you sure?",
+    text: "You will not be able to recover this imaginary file!",
+    type: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#DD6B55",
+    confirmButtonText: "Yes, delete it!",
+    closeOnConfirm: false
+  },
+  function(){
+    swal("Deleted!", "Your imaginary file has been deleted.", "success");
+  });
---
+++ tp/TP3/ex1/script.js
@@ -0,0 +1,6 @@
+const update = () =>{
+    
+    const output = document.getElementById("out")
+    const range = document.getElementById("enfants")
+    output.innerHTML=range.value;
+}
